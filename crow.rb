a6e582287e82f9d5d81a27c0385bafed39f473ea
eggrens <jaxaphone444@gmail.com>
2025-02-16 16:12:21 -0500

    added song seeking and song listing overhaul, readme update

--- crow.rb
+++ crow.rb
@@ -1,11 +1,11 @@
 require 'discordrb'
 require 'dotenv'
+require 'taglib'
 Dotenv.load
 
-Dotenv.require_keys("CROW_TOKEN", "CHANNEL_ID", "CROW_PREFIX")
+Dotenv.require_keys("CROW_TOKEN", "CROW_PREFIX")
 
-env_log = ENV['CROW_LOGGING']
-case env_log
+case ENV['CROW_LOGGING']
 when 'debug'
     log_mode = :debug
 when 'verbose'
@@ -16,36 +16,84 @@ else
     log_mode = :normal
 end
 
-bot = Discordrb::Commands::CommandBot.new log_mode: log_mode, token: ENV['CROW_TOKEN'], prefix: ENV['CROW_PREFIX']
-bot_channel = ENV['CHANNEL_ID']
+channels = ENV['CROW_CHANNELS'] ? ENV['CROW_CHANNELS'].split(',') : []
 
+options = {
+    log_mode: log_mode,
+    token: ENV['CROW_TOKEN'],
+    prefix: ENV['CROW_PREFIX'],
+    channels: channels,
+    # advanced functionality is enabled only to make parsing arguments wrapped in quotes much easier.
+    # command chaining functionality is disabled with these options!
+    advanced_functionality: true,
+    previous: '',
+    chain_delimiter: '',
+    chain_args_delim: '',
+    sub_chain_start: '',
+    sub_chain_end: ''
+}
+
+bot = Discordrb::Commands::CommandBot.new(**options)
 @queue = []
+@current_song = nil
+@time_skipped = 0
 FORMATS = [".mp3", ".flac", ".wav"]
+
+def convert_to_time(secs)
+    if secs / 3600 < 1
+        "%d:%02d" % [secs / 60 % 60, secs % 60]
+    else
+        "%d:%02d:%02d" % [secs / 3600, secs / 60 % 60, secs % 60]
+    end
+end
+
+def convert_to_secs(time)
+    time.split(':').map(&:to_i).inject(0) { |a, b| a * 60 + b}
+end
+
+class Song
+    attr_reader :filename
+    attr_reader :short
+    attr_reader :title
+    attr_reader :artist
+    attr_reader :length
+
+    def initialize(filename)
+        @filename = filename
+        @short = filename.match(/[^\/]+\.(mp3|flac|wav)/)[0]
+        TagLib::FileRef.open(filename) do |file|
+            unless file.null?
+                tag = file.tag
+                prop = file.audio_properties
+                @title = tag.title
+                @artist = tag.artist
+                @length = prop.length_in_seconds
+            end
+        end
+    end
+
+    def to_s
+        if !@title.empty? && !@artist.empty?
+            "#{@title} - #{@artist}"
+        else
+            # return title if metadata present, or return its shortened filename
+            @title.empty? ? @short : @title
+        end
+    end
+end
 
 def play_queue(voice_bot, bot)
     while !@queue.empty? do
-        f = @queue.shift
-        bot.update_status("online", f, nil, 0, false, 2)
-        voice_bot.play_file(f)
+        song = @queue.shift
+        @current_song = song
+        @time_skipped = 0
+        # Set status to "Playing [songtitle] - [songartist]"
+        bot.update_status("online", song.to_s, nil, 0, false, 2)
+        voice_bot.play_file(song.filename)
     end
+    @current_song = nil
     bot.update_status("online", nil, nil)
     "caw! finished playing"
-end
-
-def trim_quotes(s)
-    if s[0] == '"'
-        s = s.delete_prefix('"')
-    elsif s[0] == "'"
-        s = s.delete_prefix("'")
-    end
-
-    if s[-1] == '"'
-        s = s.delete_suffix('"')
-    elsif s[-1] == "'"
-        s = s.delete_suffix("'")
-    end
-
-    return s
 end
 
 bot.command(:mew, description: "mew") do |event|
@@ -74,13 +122,54 @@ bot.command(:albums, description: "lists all the directories in the albums folde
     end
 end
 
-bot.command(:queue, description: "prints the current queue") do |event|
+bot.command(:list, min_args: 1, max_args: 1, description: "list all music files inside a given album", usage: "list [album]") do |event, album|
+    next "the albums directory does not exist!" unless Dir.exist?("albums")
+    next "album #{album} not found..." unless Dir.exist?("albums/#{album}")
+    list = nil
+
+    Dir.chdir("albums/#{album}") do
+        songs = Dir.glob("**/*.{mp3,flac,wav}")
+        next "no songs found" unless songs
+
+        list = "```\nsongs in #{album}:\n"
+        songs.each_with_index do |a, i|
+            entry = "\t#{a}\n"
+            if list.size + entry.size > 1978
+                list << "(and #{songs.length - i+1} more...)\n"
+                break
+            else
+                list << entry
+            end
+        end
+        list << "```"
+    end
+
+    next "caw! this album listing is too big... (exceeded 2000 characters)" if list.size > 2000
+    return list
+end
+
+bot.command(:nowplaying, description: "lists the current song being played", aliases: [:np]) do |event|
+    next "caw! no song is being played" unless @current_song
+
+    event << "Playing:"
+    event << "**#{@current_song.to_s}**"
+    event << "`#{convert_to_time(event.voice.stream_time + @time_skipped)} / #{convert_to_time(@current_song.length)}`"
+end
+
+bot.command(:queue, description: "prints the current queue", aliases: [:q]) do |event|
     next "caw! queue is empty" if @queue.empty?
 
     list = "```\nqueue:\n"
-    @queue.each do |q|
-        list << "\t#{q}\n"
+    @queue.each_with_index do |q, i|
+        entry = "\t%3d. | %8s | %s\n" % [i+1, convert_to_time(q.length), q.to_s]
+        if list.size + entry.size > 1978
+            list << "(and #{@queue.length - i+1} more...)\n"
+            break
+        else
+            list << entry
+        end
     end
+    next "caw! this album listing is too big... (exceeded 2000 characters)" if list.size > 2000
     list << "```"
 end
 
@@ -91,11 +180,11 @@ bot.command(:shuffle, description: "shuffles the queue") do |event|
     "caw! queue shuffled :>"
 end
 
-bot.command(:play, description: "play a given file / continue playback") do |event, *filename|
+bot.command(:play, max_args: 1, description: "play a given file / continue playback", usage: 'play [path-to-file]', aliases: [:p]) do |event, file|
     voice_bot = event.voice
     next "i'm not connected to a voice channel! (use connect command)" unless voice_bot
 
-    if filename.empty?
+    unless file
         if voice_bot.playing?
             voice_bot.continue
             return "continued playback"
@@ -104,49 +193,49 @@ bot.command(:play, description: "play a given file / continue playback") do |eve
         end
     end
 
-    file = trim_quotes(filename.join(" "))
-
     next "#{file} not found..." unless File.exist?(file)
     next "#{file} is a directory..." unless !File.directory?(file)
     next "#{file} does not have .mp3/.flac/.wav extension!" unless FORMATS.include? File.extname(file)
 
-    @queue << file
-    bot.send_message(bot_channel, "caw! added #{file} to the queue")
-
+    @queue << Song.new(file)
+    event.respond "caw! added #{file} to the queue"
     play_queue(voice_bot, bot) unless voice_bot.playing?
 end
 
-bot.command(:playalbum, description: "play all music files in an album folder; add '-s' if you want it shuffled first") do |event, arg1, arg2|
+bot.command(:playalbum, min_args: 1, max_args: 2, description: "play all music files in an album folder; add '-s' if you want it shuffled first",
+ usage: 'playalbum [album] [-s]', aliases: [:pa]) do |event, *args|
     voice_bot = event.voice
+    next "i'm not connected to a voice channel! (use connect command)" unless voice_bot
     shuffle = false
 
-    if arg1 == "-s"
-        album = arg2
+    if args.count == 2
+        next "invalid parameters passed (was expecting -s)" if args[0] != "-s" && args[1] != "-s"
         shuffle = true
-    elsif arg2 == "-s"
-        album = arg1
-        shuffle = true
+        album = args[0] == "-s" ? args[1] : args[0]
     else
-        album = arg1
+        album = args[0]
     end
 
+    next "the albums directory does not exist!" unless Dir.exist?("albums")
     next "album #{album} not found..." unless Dir.exist?("albums/#{album}")
 
     files = Dir.glob("albums/#{album}/**/*.{mp3,flac,wav}")
     files.each do |f|
-        @queue << f
+        @queue << Song.new(f)
     end
 
     if shuffle
         @queue = @queue.shuffle
     end
-    bot.send_message(bot_channel, "shuffling #{album}") if shuffle
-    bot.send_message(bot_channel, "caw! added album #{album} to the queue")
+    event.respond "shuffling #{album}" if shuffle
+    event.respond "caw! added album #{album} to the queue"
     play_queue(voice_bot, bot) unless voice_bot.playing?
 end
 
 bot.command(:pause, description: "pause current song") do |event|
     voice_bot = event.voice
+    next "i'm not connected to a voice channel! (use connect command)" unless voice_bot
+
     if voice_bot.playing?
         voice_bot.pause
         "caw! paused"
@@ -157,6 +246,8 @@ end
 
 bot.command(:stop, description: "clears the queue and stops playback") do |event|
     voice_bot = event.voice
+    next "i'm not connected to a voice channel! (use connect command)" unless voice_bot
+
     if voice_bot.playing?
         @queue.clear
         voice_bot.stop_playing
@@ -168,12 +259,32 @@ end
 
 bot.command(:skip, description: "skip to the next song in queue") do |event|
     voice_bot = event.voice
+    next "i'm not connected to a voice channel! (use connect command)" unless voice_bot
+
     if voice_bot.playing?
         voice_bot.stop_playing
-        "caw! skipped"
+        "caw! skipped song"
     else
 	    "but i'm not playing anything..."
     end
+end
+
+bot.command(:seek, min_args: 1, max_args: 1, description: "seek to a later timecode in the current song being played",
+usage: "seek [H:MM:SS or M:SS]") do |event, time|
+    voice_bot = event.voice
+    next "i'm not connected to a voice channel! (use connect command)" unless voice_bot
+    next "but i'm not playing anything..." unless voice_bot.playing?
+
+    hms = time.split(':')
+    next "invalid timecode format (must be H:MM:SS or M:SS)" if hms.length > 3 || hms.length < 2
+
+    secs = convert_to_secs(time)
+    next "timecode out of bounds!" if secs > @current_song.length
+    next "timecode has been passed already (cannot go back in time)" if secs <= (voice_bot.stream_time + @time_skipped)
+
+    @time_skipped += secs - voice_bot.stream_time
+    voice_bot.skip(secs)
+    "caw! skipped to `#{convert_to_time(secs)} / #{convert_to_time(@current_song.length)}`"
 end
 
 bot.command(:disconnect, description: "disconnect Crow from the voice channel") do |event|
@@ -184,8 +295,9 @@ bot.command(:disconnect, description: "disconnect Crow from the voice channel") 
     "disconnected from #{channel.name}"
 end
 
+# note: you may want to remove this command unless you absolutely trust whoever's using it :)
 bot.command(:quit, description: "shuts down Crow") do |event|
-    bot.send_message(bot_channel, 'caw! shutting down')
+    event.respond 'caw! shutting down'
     exit
 end
 
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Crow (crow-bot)
-A Discord bot for playing music files (.mp3, .flac, or .wav) in a voice channel. Songs are added in a queue-like system, with basic controls for skipping songs in the queue, and pausing/playing. Crow can also queue all music files in a folder placed in the album directory to be played, either sequentially or shuffled.
+A Discord bot for playing music files (.mp3, .flac, or .wav) in a voice channel. Songs are added in a queue-like system, with basic controls for skipping songs in the queue, pausing/playing, and seeking to a later time in the song. Crow can also queue all music files in a folder placed in the album directory to be played, either sequentially or shuffled.
 
 ## Requirements
 
@@ -7,14 +7,34 @@ A Discord bot for playing music files (.mp3, .flac, or .wav) in a voice channel.
 - libsodium
 - libopus
 - ffmpeg
+- taglib 1.11.1 or greater, with dev headers
 
-See [discord.rb's repo](https://github.com/shardlab/discordrb) for more instructions on installing these dependencies.
+See [discord.rb's repo](https://github.com/shardlab/discordrb) for more instructions on installing libsodium and libopus.
+
+On Ubuntu/Debian, you can install taglib 1.13.1 with dev headers with the following command:
+```
+sudo apt-get install libtag1-dev
+```
 
 Required gems:
 - discordrb
 - dotenv
+- taglib-ruby
 
-You can install these gems from the Gemfile by using the Bundler gem:
+***Note: Depending on which version of taglib you have installed, you'll need to tweak your Gemfile for the bot to work.***
+
+If you're using a taglib version below 2.0, then add this to your Gemfile:
+
+```
+gem 'taglib-ruby', '< 2.0'
+```
+
+Otherwise, add this to the Gemfile:
+```
+gem 'taglib-ruby', '>= 2.0'
+```
+
+Then, install the gems using Bundler:
 ```
 bundle install
 ```
@@ -24,39 +44,51 @@ bundle install
 Before playing music, Crow must be connected to a voice channel. When you're in a voice channel, type `~connect` (replacing `~` with your desired command prefix), and Crow will connect to your channel. Likewise, use `~disconnect` to disconnect Crow from the voice channel.
 
 There are two commands for playing music:
-- `~play [path_to_file]`
+- `~play [path_to_file]` (alias: `~p`)
     - This will add a single music file to the queue when given a valid path to the file. You can use either absolute or relative path names.
-    - If the file path has spaces, you can optionally wrap it in single or double quotes.
+    - If the file path has spaces, you must wrap it in double quotes.
         - e.g. `~play "albums/my-album/this song has spaces.mp3"`
     - If no file name is given, this command causes playback of a song to continue if it was previously paused at some point.
-- `~playalbum [-s] name_of_album`
+- `~playalbum [-s] name_of_album` (alias: `~pa`)
     - This will add every music file in the given folder located in `albums/` to the queue.
-        - The name of the folder **must not** have spaces for it to be recognized, however.
+        - If the folder name has spaces, you must wrap it in double quotes.
+            - e.g. `~playalbum "my music folder"`
     - You can optionally add the flag `-s` to shuffle the queue after adding the album's songs
         - e.g. `~playalbum my-album -s` or `~playalbum -s my-album`
 
 Crow will play each song in the queue until the queue is empty. The bot's profile status will update to show which song it is currently playing.
 
-You can pause playback with `~pause`, and continue playback with `~play`. Typing `~stop` will clear all songs in the queue.
+You can also view what song Crow is currently playing with `~nowplaying` or `~np`, which displays the song title, artist, and how far Crow is in the song when you call the command. If the song file doesn't have a title specified in the metadata, the filename will be displayed instead.
+
+![A screenshot of Crow displaying the current song with `nowplaying`](readme-imgs/crow-bot-scr1.png)
+
+You can pause playback with `~pause`, and continue playback with `~play`. Typing `~stop` will clear all songs in the queue. You can also seek to a specified timecode in the song by using `~seek [H:MM:SS or MM:SS]`, but note that Crow cannot go back in time (i.e. the timecode has already been passed).
 
 Typing `~help` will bring up a list of the available commands, and you can use `~help [command_name]` to get help for a specific command.
 
 Other commands:
 - `~albums` - lists all albums in the `albums/` directory
-- `~queue` - lists all songs in the queue
+- `~list my-album` - lists all songs in an album in the `albums/` directory
+
+![A screenshot of Crow displaying the current song with `nowplaying`](readme-imgs/crow-bot-scr3.png)
+
+- `~queue` or `~q` - lists all songs in the queue with length, title and artist
+
+![A screenshot of Crow displaying the current song with `nowplaying`](readme-imgs/crow-bot-scr2.png)
+
 - `~skip` - skips to the next song in the queue
 - `~shuffle` - shuffles the order of songs currently in the queue
 - `~quit` - terminates the program
 - `~mew` - Crow says "mew!" :>
 
-
 ## Running
 
 Before running Crow, you need to make a `.env` file in the program's directory. The following variables can be defined in this file:
-- `CROW_TOKEN`  ---> your Discord bot's token (***required***)
-- `CHANNEL_ID`  ---> the ID of a text channel for Crow to send statuses to (***required***)
-- `CROW_PREFIX` ---> the command prefix to use for commands (***required***)
-- `CROW_LOGGING` ---> the logging mode to use (default is `normal`)
+- `CROW_TOKEN`    ---> your Discord bot's token (***required***)
+- `CROW_PREFIX`   ---> the command prefix to use for commands (***required***)
+- `CROW_CHANNELS` ---> list of comma-separated channel IDs Crow can be used in (omit for Crow to be usable in any channel)
+    - you can find a channel's ID by enabling Developer Mode in Discord's settings -> right-click channel -> Copy Channel ID
+- `CROW_LOGGING`  ---> the logging mode to use (default is `normal`)
     - other logging modes:
         - `debug` --> logs everything
         - `verbose` --> logs everything exccept debug messages
@@ -66,8 +98,8 @@ Before running Crow, you need to make a `.env` file in the program's directory. 
 Example `.env` file:
 ```
 CROW_TOKEN=MY_BOT_TOKEN
-CHANNEL_ID=TEXT_CHANNEL_ID
 CROW_PREFIX='~'
+CROW_CHANNELS=123,456,789
 CROW_LOGGING='verbose'
 ```
 
